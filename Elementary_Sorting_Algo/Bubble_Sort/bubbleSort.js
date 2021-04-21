ce86e012f1b2dc26a0b4b5b4a08de204f9fb8fdb
Bryan <bryanjguilas@gmail.com>
2021-04-21 11:28:11 -0700

    Commit

--- Elementary_Sorting_Algo/Bubble_Sort/bubbleSort.js
+++ Elementary_Sorting_Algo/Bubble_Sort/bubbleSort.js
@@ -87,3 +87,26 @@ function bubbleSort(arr) {
 }
 
 bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
+
+//WITH CONSOLE LOGS
+const bubbleSort = arr => {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+	noSwaps = true;
+	for (let j = 0; j < i - 1; j++) {
+	  console.log(arr, arr[j], arr[j + 1]);
+	  if (arr[j] > arr[j + 1]) {
+		let temp = arr[j];
+		console.log('TEMP:', temp)
+		arr[j] = arr[j + 1];
+		arr[j + 1] = temp;
+		noSwaps = false;
+	  }
+	}
+	if(noSwaps) break;
+  }
+  return arr;
+};
+
+console.log(bubbleSort([5, 4, 7, 3, 2, 1, 6]));
+
