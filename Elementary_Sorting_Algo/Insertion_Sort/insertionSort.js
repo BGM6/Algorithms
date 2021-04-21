dff6497d11fc492756fb69acd1bf26610383abe1
Bryan <bryanjguilas@gmail.com>
2021-04-21 11:56:29 -0700

    commit

--- Elementary_Sorting_Algo/Insertion_Sort/insertionSort.js
+++ Elementary_Sorting_Algo/Insertion_Sort/insertionSort.js
@@ -1,13 +1,13 @@
-// function insertionSort(arr) {
-//   for(let i = 0; i < arr.length; i++) {
-// 	let currentVal = arr[i];
-// 	for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-// 	  arr[j+1] = arr[j];
-// 	}
-// 	arr[j+1] = currentVal;
-//   }
-//   return arr;
-// }
+function insertionSort(arr) {
+  for(let i = 0; i < arr.length; i++) {
+	let currentVal = arr[i];
+	for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+	  arr[j+1] = arr[j];
+	}
+	arr[j+1] = currentVal;
+  }
+  return arr;
+}
 
 /*
 We loop through the arr starting at 1 through the length of the arr
@@ -25,22 +25,22 @@ Then we set arr[j+1] to equal currentNumber
 Once the while loop is false we return the arr;
 */
 
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-
-	let key = arr[i];
-	let j = i - 1;
-
-	while (j >= 0 && arr[j] > key) {
-	  arr[j + 1] = arr[j];
-	  j = j - 1;
-	}
-	arr[j + 1] = key;
-  }
-  return arr;
-}
-
-const res = insertionSort([5, 3, 1, 4, 6]);
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//
+// 	let key = arr[i];
+// 	let j = i - 1;
+//
+// 	while (j >= 0 && arr[j] > key) {
+// 	  arr[j + 1] = arr[j];
+// 	  j = j - 1;
+// 	}
+// 	arr[j + 1] = key;
+//   }
+//   return arr;
+// }
+//
+// const res = insertionSort([5, 3, 1, 4, 6]);
 
 /*
 [5, 3, 1, 4, 6]
--- practice.js
+++ practice.js
@@ -1,21 +1,37 @@
-const bubbleSort = arr => {
-  let noSwaps;
-  for (let i = arr.length; i > 0; i--) {
-	noSwaps = true;
-	for (let j = 0; j < i - 1; j++) {
-	  console.log(arr, arr[j], arr[j + 1]);
-	  if (arr[j] > arr[j + 1]) {
-		let temp = arr[j];
-		console.log('TEMP:', temp)
-		arr[j] = arr[j + 1];
-		arr[j + 1] = temp;
-		noSwaps = false;
-	  }
+// 'use strict';
+// const insertionSort = arr => {
+//   for (let i = 0; i < arr.length; i++) {
+// 	let currentVal = arr[i];
+// 	for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+// 	  arr[j + 1] = arr[j];
+//
+// 	}
+// 	arr[j + 1] = currentVal;
+//   }
+//   return arr;
+// };
+//
+// console.log(insertionSort([14, 33, 27, 10, 35, 19, 42, 44]));
+
+function insertionSort(arr) {
+  //We start at index 1 of the array
+  for (let i = 1; i < arr.length; i++) {
+	//Create a variable equal to arr[i]
+	let val = arr[i];
+	//define j to be the index before i;
+	//first iteration j is at 0 and i is at 1
+	let j = i - 1;
+	//While loop if the condition to continue
+	//as long as j is >= 0 and arr[j] is > than val
+	while (j >= 0 && arr[j] > val) {
+	  arr[j + 1] = arr[j];
+	  j = j - 1;
 	}
-	if(noSwaps) break;
+	arr[j + 1] = val;
+
   }
   return arr;
-};
+}
 
-console.log(bubbleSort([5, 4, 7, 3, 2, 1, 6]));
-
+console.log(insertionSort([14, 33, 27, 10, 35, 19, 42, 44]));
+                            j    i
