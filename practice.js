b83c6a07b41611bd4b29b4b48f02faa24dad8aa5
bguilas <bryanjguilas@gmail.com>
2021-05-03 19:49:50 -0700

    doubly linked list

--- practice.js
+++ practice.js
@@ -1,15 +1,1 @@
 'use strict';
-
-//Push Method
-
-//Pop Method
-//if there is no head return undefined
-//need current to start at the head and tail to equal current
-//while there is still a current;
-// newTail === current and current === current.next;
-//if the while loop ends
-//set this.tail to newTail
-// We need to remove the old tail by setting next tail to null/ this.tail.next === null
-//then we need to decrement the length
-//check if the length is === 0 then set the head and the tail to null;
-//return current;
