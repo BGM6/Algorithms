ef7e0e3a628f8ee8b786b3dc7f8cb4abf6a642cf
bguilas <bryanjguilas@gmail.com>
2021-05-09 20:59:47 -0700

    binary search trees

---
+++ Data_Structures/Binary_Search_Trees/binarySearchTree.js
@@ -0,0 +1,125 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    //INSERT
+    insert(val) {
+        let newNode = new Node(val);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.val) return undefined;
+                if (val < current.val) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    //FIND returns the actual node
+    find(val) {
+        if (this.root === null) return false;
+        let current = this.root;
+        let found = false;
+        while (current && !found) {
+            if (val < current.val) {
+                current = current.left;
+            } else if (val > current.val) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return false;
+        return current;
+    }
+
+    //Contains returns true or false;
+    contains(value) {
+        if (this.root === null) return false;
+        let current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(7);
+tree.insert(20);
+tree.insert(15);
+console.log(tree);
+
+
+// let tree = new BinarySearchTree();
+// tree.root = new Node(10)
+// tree.root.right = new Node(15)
+// tree.root.right = new Node(12)
+// tree.root.left = new Node(5)
+// tree.root.left = new Node(3)
+//
+
+/*
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        let newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+}
+*/
